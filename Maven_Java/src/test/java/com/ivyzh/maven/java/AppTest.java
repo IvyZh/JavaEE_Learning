33ac40e5d57e3c807fab8886b4fcbc1eaf14b9df
cutiefin <abstractaccount@163.com>
2019-05-10 19:10:40 +0800

    finish maven_java.

---
+++ Maven_Java/src/test/java/com/ivyzh/maven/java/AppTest.java
@@ -0,0 +1,20 @@
+package com.ivyzh.maven.java;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
